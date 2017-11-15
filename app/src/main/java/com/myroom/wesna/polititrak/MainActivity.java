97d390d41b96a83a857db750fbe49e8230590a96
mazkatraz <mazkatraz@yahoo.com>
2017-11-14 22:45:34 -0500

    Added Material search bar.

--- app/src/main/java/com/myroom/wesna/polititrak/MainActivity.java
+++ app/src/main/java/com/myroom/wesna/polititrak/MainActivity.java
@@ -1,13 +1,42 @@
 package com.myroom.wesna.polititrak;
 
+import android.support.v7.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialSearchBar materialSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inflate the custom layout for the action bar
+        final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
+                R.layout.action_bar,
+                null,
+                true
+        );
+
+        //Set custom view for the action bar
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setCustomView(actionBarLayout);
+
+            //Set the color of the action bar
+            actionBar.setBackgroundDrawable(new ColorDrawable(
+                    getResources().getColor(R.color.colorPrimary)
+            ));
+        }
+
+        materialSearchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        materialSearchBar.setNavButtonEnabled(true);
     }
 }
