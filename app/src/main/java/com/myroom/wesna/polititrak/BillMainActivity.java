d61f2f2f5c17df24b5f7ec80ff207c55e366cb4e
mazkatraz <mazkatraz@yahoo.com>
2017-11-18 22:03:59 -0500

    Removed the View Pager and the Fragments. Added an API call to get recent bills. Added internet permissions. Added Network utils.

--- app/src/main/java/com/myroom/wesna/polititrak/BillMainActivity.java
+++ app/src/main/java/com/myroom/wesna/polititrak/BillMainActivity.java
@@ -1,108 +1,86 @@
 package com.myroom.wesna.polititrak;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BillMainActivity extends FragmentActivity {
-    private ViewPager viewPager;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.myroom.wesna.polititrak.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class BillMainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill_main);
 
-        PagerAdapter pagerAdapter
-                = new PagerAdapter(getSupportFragmentManager(), this);
+        textView = (TextView) findViewById(R.id.testTextView);
 
-        //Set adapter to view pager
-        viewPager = (ViewPager) findViewById(R.id.pager);
-        viewPager.setAdapter(pagerAdapter);
-
-        //Set up tab layout
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);
-
-
-        //Select the corresponding tab when the user swipes between pages
-        viewPager.addOnPageChangeListener(
-                new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        ActionBar actionBar = getActionBar();
-                        if(actionBar != null) {
-                            actionBar.setSelectedNavigationItem(position);
-                        }
-                    }
-                }
+        //Inflate the custom layout for the action bar
+        final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
+                R.layout.action_bar,
+                null,
+                true
         );
-    }
-}
 
-/**
- * Page adapter for the view pager for Bills
- */
-class PagerAdapter extends FragmentPagerAdapter {
-    private Context context;
+        //Set custom view for the action bar
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setCustomView(actionBarLayout);
 
-    /**
-     * Fragment Pager Adapter constructor
-     * @param fm The fragment manager
-     * @param context The context of the view pager
-     */
-    PagerAdapter(FragmentManager fm, Context context) {
-        super(fm);
-        this.context = context;
-    }
-
-    /**
-     * Gets the appropriate fragment bases on position
-     * @param position The position of the fragment in the view pager
-     * @return The fragment bases on the position
-     */
-    @Override
-    public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return Fragment.instantiate(context, LocalBillsFragment.class.getName());
-            case 1:
-                return Fragment.instantiate(context, NationalBillsFragment.class.getName());
-
-        }
-        return null;
-    }
-
-    /**
-     * Sets the titles for the tabs in the Bill view pager
-     * @param position The position of the fragment in the view pager
-     * @return The title char sequence
-     */
-    @Override
-    public CharSequence getPageTitle(int position) {
-        switch (position) {
-            case 0:
-                return context.getString(R.string.local_bills_tab);
-            case 1:
-                return context.getString(R.string.national_bills_tab);
+            //Set the color of the action bar
+            actionBar.setBackgroundDrawable(new ColorDrawable(
+                    getResources().getColor(R.color.colorPrimary)
+            ));
         }
 
-        return null;
+        //Set up search bar and add the nav button
+        MaterialSearchBar materialSearchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        materialSearchBar.setNavButtonEnabled(true);
+
+        getRecentBills();
     }
 
-    /**
-     * The amount of fragments the view pager holds
-     * @return The amount of fragments in view pager
-     */
-    @Override
-    public int getCount() {
-        return 2;
+    private void getRecentBills() {
+        URL getRecentBillsURL = NetworkUtils.buildRecentBillsUrl();
+        new GetRecentBillsTask().execute(getRecentBillsURL);
+    }
+
+    //TODO: Make this static to prevent leaks
+    public class GetRecentBillsTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL recentBillsUrl = urls[0];
+            String recentBillsResults = null;
+            try {
+                recentBillsResults = NetworkUtils.getResponseFromHttpUrl(recentBillsUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return recentBillsResults;
+        }
+
+        @Override
+        protected void onPostExecute(String recentBillsResults) {
+            if(recentBillsResults == null) {
+                Toast.makeText(getBaseContext(), "Dun Goofed", Toast.LENGTH_SHORT).show();
+                //TODO: Handle this properly.
+            } else {
+                textView.setText(recentBillsResults);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/myroom/wesna/polititrak/utilities/NetworkUtils.java
@@ -0,0 +1,64 @@
+package com.myroom.wesna.polititrak.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    /**
+     * Builds the URL to get the recent bills from the API
+     * @return A URL to the get-recent-bills API call
+     */
+    public static URL buildRecentBillsUrl() {
+        String baseUrl = "https://api.propublica.org/congress/v1/115/both/bills/introduced.json";
+
+        Uri builtUri = Uri.parse(baseUrl).buildUpon()
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        String xApiKey = "X-API-Key";
+        String apiKey = "abc123";
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.addRequestProperty("x-api-key", apiKey);
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
